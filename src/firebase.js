bbdfa183109cf7cb651aadc5491a76eeb439998c
Jai Misra <misrajai01@gmail.com>
2022-10-04 01:29:08 -0600

    Install npm package for Firebase and add init code and configuration for the Realtime Database to a firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  databaseUrl: process.env.REACT_APP_FIREBASE_DATABASEURL,
+};
+
+// Initialize Firebase
+const fireBaseApp = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(fireBaseApp);
+
+export { fireBaseApp, database };
